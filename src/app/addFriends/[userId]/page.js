50edb81d9d35a698d8dd296e6bc928e4e34c029b
aditya-raj2230 <aditya17025@gmail.com>
2024-06-29 13:13:07 +0530

    tried again

--- src/app/addFriends/[userId]/page.js
+++ src/app/addFriends/[userId]/page.js
@@ -1,10 +1,11 @@
 'use client';
 import React from "react";
 import { useRouter } from "next/navigation";
-import SelectedUserProfile from "@/components/SelectedUserProfile";
+
 import UserPosts from "@/components/UserPosts";
 import Reviews from "@/components/Reviews";
 import AuthGuard2 from "@/components/LoggedOutAuthGaurad";
+import SelectedUserProfile from "@/components/SelectedUserProfile";
 
 const Page = ({ params }) => {
   const { userId } = params;
