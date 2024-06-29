35898220cf371b0fb4a4f9ed471466de25af8b7e
aditya-raj2230 <aditya17025@gmail.com>
2024-06-29 12:59:17 +0530

    final

--- src/app/verify-email/page.js
+++ src/app/verify-email/page.js
@@ -1,10 +1,11 @@
-import VerifyEmail from '@/components/VerifyEmail';
+import CheckEmail from '@/components/CheckEmail';
+
 
 export default function VerifyEmailPage() {
   return (
     <div>
       <h1>Verify Email</h1>
-      <VerifyEmail />
+      <CheckEmail/>
     </div>
   );
 }
