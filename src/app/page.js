ff3e11c3ba2738dba98b5d83f0374c3052619082
aditya-raj2230 <aditya17025@gmail.com>
2024-09-02 23:31:04 +0530

    check2

--- src/app/page.js
+++ src/app/page.js
@@ -5,9 +5,10 @@ import { useRouter } from "next/navigation";
 import UserProfile from "@/components/Profile";
 import GroupList from "@/components/GroupList";
 import { useAuth } from "./context/AuthContext";
-import UploadVehicle from "@/components/UploadVehicle"; // Import the UploadVehicle component
+import UploadVehicle from "@/components/UploadVehicle";
 import { doc, getDoc } from "firebase/firestore";
 import { db } from "@/app/firebase/config";
+import Hero from "@/components/Hero";
 
 const Page = () => {
   const router = useRouter();
@@ -15,7 +16,7 @@ const Page = () => {
   const userId = user?.uid;
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [showUploadVehicle, setShowUploadVehicle] = useState(false); // State to control showing UploadVehicle component
+  const [showUploadVehicle, setShowUploadVehicle] = useState(false);
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -33,6 +34,8 @@ const Page = () => {
         } finally {
           setLoading(false);
         }
+      } else {
+        setLoading(false);
       }
     };
 
@@ -45,20 +48,27 @@ const Page = () => {
 
   return (
     <div className="flex flex-col md:flex-row min-h-screen mb-20">
-      <div className="w-full md:w-1/3 p-4">
-        <UserProfile selectedUserId={userId} />
-        <button
-          onClick={() => setShowUploadVehicle(true)} // Show the UploadVehicle component on button click
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mb-4"
-        >
-          Upload Vehicle
-        </button>
-        {/* Conditionally render the UploadVehicle component */}
-        {showUploadVehicle && <UploadVehicle userData={userData} userId={userId} />}
-      </div>
-      <div className="w-full md:w-2/3 p-4">
-        <GroupList />
-      </div>
+      {!userId ? (
+        <div className="w-full">
+          <Hero />
+        </div>
+      ) : (
+        <>
+          <div className="w-full md:w-1/3 p-4">
+            <UserProfile selectedUserId={userId} />
+            <button
+              onClick={() => setShowUploadVehicle(true)} // Show the UploadVehicle component on button click
+              className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 mb-4"
+            >
+              Upload Vehicle
+            </button>
+            {showUploadVehicle && <UploadVehicle userData={userData} userId={userId} />}
+          </div>
+          <div className="w-full md:w-2/3 p-4">
+            <GroupList />
+          </div>
+        </>
+      )}
     </div>
   );
 };
